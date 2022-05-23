d30dbf405295b28dc962ea61d4a677920e4bd7eb
foolishfish <2295339588@qq.com>
2022-05-23 23:39:53 +0800

    test提交

--- index.js
+++ index.js
@@ -14,48 +14,52 @@ app.use(logger);
 
 // 首页
 app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // 更新计数
 app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
+    const { action } = req.body;
+    if (action === "inc") {
+        await Counter.create();
+    } else if (action === "clear") {
+        await Counter.destroy({
+            truncate: true,
+        });
+    }
+    res.send({
+        code: 0,
+        data: await Counter.count(),
     });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
+});
+
+app.get("/api/test", (req, res) => {
+    res.send('这里是一个测试')
 });
 
 // 获取计数
 app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
+    const result = await Counter.count();
+    res.send({
+        code: 0,
+        data: result,
+    });
 });
 
 // 小程序调用，获取微信 Open ID
 app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
+    if (req.headers["x-wx-source"]) {
+        res.send(req.headers["x-wx-openid"]);
+    }
 });
 
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
+    await initDB();
+    app.listen(port, () => {
+        console.log("启动成功", port);
+    });
 }
 
 bootstrap();
